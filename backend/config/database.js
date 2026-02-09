5cf2de6c2f2783e7e6ec6fd6894dfae6fc5d91cf
Rakshith <poojaryrakshith986@gmail.com>
2026-02-09 15:35:10 +0530

    first comkit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
+    console.log("MONGO",process.env.MONGO_URI);
     mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Mongoose Connected");
