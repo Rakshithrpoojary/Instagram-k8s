648f3c931bc1c9bbc8626537e6eef43ce8ac59ad
Rakshith <poojaryrakshith986@gmail.com>
2026-02-19 16:25:49 +0530

    Added main.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "insta-tf-state-bkt"
-    key            = "vpc/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
---
+++ Terraform/main.tf
@@ -0,0 +1,74 @@
+
+data "aws_vpc" "insta_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["Rakshith"]
+  }
+}
+
+resource "aws_subnet" "insta-subnet" {
+  vpc_id                  = data.aws_vpc.insta_vpc.id
+  cidr_block              = var.subnet.cidr
+  availability_zone       = var.subnet.availability_zone
+  tags                    = var.subnet.tags
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "sg" {
+  vpc_id   = data.aws_vpc.insta_vpc.id
+  for_each = var.sg
+  name     = each.key
+  dynamic "ingress" {
+    for_each = each.value.port
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_internet_gateway" "insta_igw" {
+  vpc_id = data.aws_vpc.insta_vpc.id
+  tags = {
+    Name = "Insta_igw"
+  }
+
+}
+resource "aws_route_table" "insta_rt" {
+  vpc_id = data.aws_vpc.insta_vpc.id
+  tags = {
+    Name = "Insta_rt"
+  }
+}
+
+resource "aws_route" "insta_rts" {
+  route_table_id         = aws_route_table.insta_rt.id
+  gateway_id             = aws_internet_gateway.insta_igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "insta_rt_assoc" {
+  route_table_id = aws_route_table.insta_rt.id
+  subnet_id      = aws_subnet.insta-subnet.id
+}
+resource "aws_instance" "instance" {
+  for_each               = var.instance
+  ami                    = each.value.ami
+  instance_type          = each.value.type
+  subnet_id              = aws_subnet.insta-subnet.id
+  key_name               = "jenkins-key"
+  vpc_security_group_ids = [aws_security_group.sg[each.value.sg].id]
+  root_block_device {
+    volume_size = each.value.storage
+    volume_type = each.value.storagetype
+  }
+  tags = each.value.tags
+}
